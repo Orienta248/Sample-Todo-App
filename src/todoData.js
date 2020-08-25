27c65e81cd39845e004dca442a735f7fb88bd442
Orienta <orienta@Users-MacBook-Pro.local>
2020-08-25 13:39:56 +0100

    WIP: Todo App

---
+++ src/todoData.js
@@ -0,0 +1,29 @@
+const todoData = [
+    {
+        id:1,
+        text:'build react app',
+        completed:'true'
+    },
+    {
+        id:2,
+        text:'take full-stack reactjs',
+        completed:'false'
+    },
+    {
+        id:3,
+        text:'build more apps',
+        completed:'false'
+    },
+    {
+        id:4,
+        text:'finish all tutorials',
+        completed:'true'
+    },
+    {
+        id:5,
+        text:'execute more konga task',
+        completed:'true'
+    },
+]
+
+export default todoData
